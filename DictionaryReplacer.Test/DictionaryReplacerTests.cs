dc0dacd0ed39a163b53e1e7248c8e9b6bf89bd69
Sphephelo Gumede <Sphephelo.Gumede@kcpivot.co.za>
2022-02-10 09:27:48 +0200

    Added Dictionary Replacer One files

---
+++ DictionaryReplacer.Test/DictionaryReplacerTests.cs
@@ -0,0 +1,116 @@
+using NUnit.Framework;
+using DictionaryReplacer;
+using System.Collections.Generic;
+
+namespace DictionaryReplacer.Test
+{
+    public class DictionaryReplacerTests
+    {
+        private DictionaryReplacer _dictionaryReplacer;
+        [SetUp]
+        public void Setup()
+        {
+            _dictionaryReplacer = new DictionaryReplacer();
+        }
+
+        [Test]
+        public void GivenAWordPrefixedWithADollarSigns_WhenExtractingKeys_ShouldReturnKeys()
+        {          
+                // Arrange
+                string expected = "name";
+
+                // Act
+                string result = _dictionaryReplacer.TrimDollarSigns("$name$");
+
+                // Assert
+                Assert.AreEqual(expected, result);            
+        }
+        [Test]
+        public void GivenAStringWithWordPrefixedWithADollarSign_WhenExtractingKeys_ShouldReturnAListOfKeys()
+        {
+            // Arrange
+            List<string> expected= new List<string>() { "temp","name"};
+            string[] splitwords = { "$temp$", "here comes the name", "$name$" };
+            // Act
+            List <string> result = _dictionaryReplacer.GetKeys(splitwords);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenADictionaryAndAListOfKeys_WhenFindingValuesMatchingKeys_ShouldReturnAListOfValues()
+        {
+            // Arrange
+            List<string> expected = new List<string>() { "temporary", "John Doe" };
+            List<string> keys = new List<string>() { "temp","name"};
+            Dictionary<string, string> dictionary = new Dictionary<string, string>() { { "temp", "temporary" },{ "name", "John Doe" } };
+
+            // Act
+            List<string> result = _dictionaryReplacer.FindValuesInDictionary(dictionary,keys);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenAnArrayWithTheOriginalStringBrokenUpAndAlistOfValues_WhenReplacingKeysWithMatchingValues_ShouldReturnAstringWithTheWordsReplaced()
+        {
+            // Arrange
+            string expected = "temporary here comes the name John Doe";
+            string[] wordList = { "$temp$","here comes the name","$name$" };
+            List<string> valuesFound = new List<string>() { "temporary", "John Doe" };
+
+            // Act
+            string result = _dictionaryReplacer.ReplaceDictionary(wordList, valuesFound);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenAStringWithOneWordAndADictionsrt_WhenReplacingKeysWithMatchingValues_ShouldReturnAstringWithTheWordsReplaced()
+        {
+            // Arrange
+            string expected = "temporary";
+            Dictionary<string, string> dictionary = new Dictionary<string, string>() { { "temp", "temporary" } };
+            string sentence = "$temp$";
+
+            // Act
+            string result = _dictionaryReplacer.Replace(sentence,dictionary);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenAStringAndADictionary_WhenReplacingKeysWithMatchingValues_ShouldReturnAstringWithTheWordsReplaced()
+        {
+            // Arrange
+            string expected = "temporary here comes the name John Doe";
+            Dictionary<string, string> dictionary = new Dictionary<string, string>() { { "temp", "temporary" },{"name", "John Doe" } };
+            string sentence = "$temp$ here comes the name $name$";
+
+            // Act
+            string result = _dictionaryReplacer.Replace(sentence, dictionary);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GivenAnEmptyStringAndADictionary_WhenReplacingKeysWithMatchingValues_ShouldReturnAstringWithTheWordsReplaced()
+        {
+            // Arrange
+            string expected = "";
+            Dictionary<string, string> dictionary = new Dictionary<string, string>() {  };
+            string sentence = "";
+
+            // Act
+            string result = _dictionaryReplacer.Replace(sentence, dictionary);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ DictionaryReplacer/DictionaryReplacer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryReplacer
+{
+    public class DictionaryReplacer
+    {
+        public string Replace(string sentence, Dictionary<string,string> dictionary)
+        {
+            if(string.IsNullOrEmpty(sentence) || dictionary.Equals(null) ){
+                return string.Empty;
+            }
+            string[] wordList = sentence.Split(' ');
+            List<string> keys = GetKeys(wordList);
+            List<string> values = FindValuesInDictionary(dictionary, keys);
+            string replacedWords = ReplaceDictionary(wordList, values);
+            return replacedWords;
+        }
+
+        public string TrimDollarSigns(string word)
+        {            
+            return word.Replace('$', ' ').Trim(); 
+        }
+
+        public List<string> GetKeys(string[] wordList)
+        {
+            List<string> keys = new List<string>();
+            foreach(string word in wordList)
+            {
+                if (word.Contains("$"))
+                {
+                    keys.Add(TrimDollarSigns(word).Trim());
+                }
+            }
+            return keys;
+         }
+
+        public List<string> FindValuesInDictionary(Dictionary<string,string> dictionary, List<string> keys)
+        {
+            List<string> valuesFound = new List<string>();
+            foreach (string key in keys)
+            {
+                valuesFound.Add(dictionary[key]);
+            }
+            return valuesFound;
+        }
+
+        public string ReplaceDictionary(string[]wordList, List<string>valuesFound)
+        {
+            int countValues =0;
+            string ReplacedWords = "";
+            for(int i=0;i<wordList.Length;i++)
+            {
+                if (wordList[i].Contains("$"))
+                {
+                    ReplacedWords += " "+valuesFound[countValues];
+                    countValues++;
+                }
+                else
+                {
+                   ReplacedWords += " "+ wordList[i];
+                }
+            }
+            return ReplacedWords.Trim();
+        }
+    }
+}
